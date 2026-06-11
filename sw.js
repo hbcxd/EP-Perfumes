d1870cda4a94d0f3da36ed2d5b09219f160739f5
hbcxd <victorjoseescobar.eq@gmail.com>
2026-06-11 01:17:51 -0400

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'ep-perfumes-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './LOGO.png',
+  './perfumes.png'
+];
+
+// Instalar el Service Worker y guardar archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Interceptar peticiones y usar caché si no hay internet (ideal para cargar la app rápido)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
